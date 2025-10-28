96c382b4f055f5fa93f42a3188353d80db88bd9d
Shibayu-11 <shibayusuke11@gmail.com>
2025-10-29 01:12:10 +0900

    Update metadata service

--- app/services/metadata.ts
+++ app/services/metadata.ts
@@ -6,4 +6,7 @@ export const metadata: Metadata = {
   title: "サービス一覧｜株式会社リクステップ",
   description:
     "株式会社リクステップが提供する各種サービスをご紹介。HP制作、アプリ開発、採用支援、システム開発、アート事業、飲食店支援など多岐にわたり対応。ビジネス課題を一気通貫で解決します。",
+    alternates: {
+      canonical: "https://recustep.com/services",
+    },
 };
